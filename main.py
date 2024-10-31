d25027b5a3f69f1a4fabb73696f4980bf4429f8b
b8niiii <120273016+b8niiii@users.noreply.github.com>
2024-10-31 16:42:57 +0100

    Created a ipynb to manage data easier. Started plotting

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import torch
+import numpy as np
 import streamlit as st
 animal_class = pd.read_csv("animals_data\class.csv") # this is the actual division
 zoo = pd.read_csv("animals_data\zoo.csv") # data we should work on 
@@ -11,9 +12,14 @@ columns = zoo.columns
 'aquatic', 'predator', 'toothed', 'backbone', 'breathes', 'venomous',
 'fins', 'legs', 'tail', 'domestic', 'catsize', 'class_type' """
 
-plot = zoo.boxplot("legs", grid = False, vert = True, patch_artist = True )
+plot = zoo.boxplot("legs", grid = False, vert = False, patch_artist = True )
 plt.show()
 
 
-
+def percentage(column_name):
+    zoo[column_name].sum() / len(zoo) 
+for element in columns:
+    perc = percentage(element)
+    print(f"{element}, {perc}: of the dataset's animals ")
+    plot = plt.bar(["No","Yes"],[1-perc, perc], color = 'green' )
 
